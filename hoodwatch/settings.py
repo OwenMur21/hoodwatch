936ff047ba5ae1f2526214c72c01d2ec7a3de239
Owenmur21 <owenmuriithi@gmail.com>
2018-10-16 17:12:27 +0300

    Adds bootstrap to application

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gang'
+    'gang',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
