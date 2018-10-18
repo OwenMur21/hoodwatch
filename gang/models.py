00fce9deca34f185ad298ed932f984b7fde91e47
Owenmur21 <owenmuriithi@gmail.com>
2018-10-18 09:55:42 +0300

    Creates neighbour class and functions

--- gang/models.py
+++ gang/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Neighbour(models.Model):
+    """
+    Class that defines neighbourhood details
+    """
+    name = models.CharField(max_length = 30)
+    location = models.CharField(max_length = 30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def create_neigborhood(self):
+        self.save()
+
+    def delete_neigborhood(self):
+        self.delete()
+
+    @classmethod
+    def get_hood_by_id(cls, id):
+            hood = Neighbour.objects.get(id=id)
+            return hood
+
+    
+
+
+
