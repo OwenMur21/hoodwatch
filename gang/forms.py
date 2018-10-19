6e3f8270c02d251cf525872c793c1ed3ce6284d1
Owenmur21 <owenmuriithi@gmail.com>
2018-10-19 11:40:39 +0300

    Creates Createhood form

--- gang/forms.py
+++ gang/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Neighbour
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
@@ -9,3 +10,10 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
         
+class CreateHoodForm(forms.ModelForm):
+    """
+    Model form class to create a neighbourhood
+    """
+    class Meta:
+        model = Neighbour
+        exclude =['user'] 
